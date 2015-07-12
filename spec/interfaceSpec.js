b3e4df7c3df007dd12289a9dd46d97d564edf7b9
zhangxu <xuzhang@thoughtworks.com>
2015-07-12 23:39:46 +0800

    add use case for how to use interface in javascript

--- spec/interfaceSpec.js
+++ spec/interfaceSpec.js
@@ -1,1 +1,11 @@
 var Composite = new Interface('Composite', ['add', 'remove', 'getChild']);
+
+var FormItem = new Interface('FormItem', ['save']);
+
+var CompositeForm = function() {
+
+};
+
+var complexedForm = new CompositeForm();
+
+Interface.ensureImplements(complexedForm, Composite);
